70c2f1e2dc13eb4b117581d12f0d8c551cc3056a
Dibbo T <dibbotripura7@gmail.com>
2023-09-16 19:39:28 +0600

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css';
+
+
+const Home = () => {
+
+
+    return(
+        <div>
+            <h2>Movie Website</h2>
+            <h4>Choose Your Favourite Movie </h4>
+        </div>
+    )
+
+}
+
+
+
+export default Home;
